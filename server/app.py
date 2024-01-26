480423d7ed454aade867fea857a9fe4997f9e062
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2024-01-26 19:42:20 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -8,7 +6,7 @@ from models import db, Bakery, BakedGood
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False  # Corrected attribute name
 
 migrate = Migrate(app, db)
 
@@ -20,19 +18,65 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()  # Corrected method name 'query()' to 'query.all()'
+    
+    bakery_list = [bakery.to_dict() for bakery in bakeries]  # Convert each Bakery object to a dictionary
+    
+    response = make_response(
+        jsonify(bakery_list),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)  # Corrected method name 'query()' to 'query.get()'
+    
+    if bakery:
+        bakery_dict = bakery.to_dict()
+        
+        response = make_response(
+            jsonify(bakery_dict),
+            200
+        )
+        response.headers["Content-Type"] = "application/json"
+        return response
+    else:
+        return 'Bakery not found', 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+
+    response = make_response(
+        jsonify(baked_goods_list),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+     
+    prices = [baked_good.price for baked_good in most_expensive_good]
+
+    highest_price = max(prices)
+    
+    
+    response = make_response(
+        jsonify(highest_price.to_dict()),
+        200
+    )
+
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
